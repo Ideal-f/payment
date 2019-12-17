8b8a711c5bd5072e0978e4e6a323d33802f13d40
fenglixiang <2425379648@qq.com>
2019-12-17 18:50:23 +0800

    初始化

---
+++ src/Pay.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Ideal-f
+ * Date: 2019/12/17
+ * Time: 21:39
+ */
+
+namespace Payment;
+
+
+class Pay
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
